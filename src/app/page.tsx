b30e212167e3639c67cdce871ff8678f58ba6b0a
Matheus Oliveira <118443175+matheus-dev-fullstack@users.noreply.github.com>
2024-02-24 23:31:51 +0000

    Finalizando Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,13 @@
-import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward } from 'lucide-react';
+import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize, Maximize2 } from 'lucide-react';
 import Image from 'next/image';
+import { Sidebar } from '../components/Sidebar';
+import { Footer } from '../components/Footer';
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-6">
-          <div className='flex items-center gap-2'>
-            <div className='w-3 h-3 bg-red-500 rounded-full' />
-            <div className='w-3 h-3 bg-yellow-500 rounded-full' />
-            <div className='w-3 h-3 bg-green-500 rounded-full' />
-          </div>
-          <nav className='space-y-5 mt-10'>
-            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'><HomeIcon />Home</a>
-            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'><Search />Search</a>
-            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'><Library />Your Library</a>
-          </nav>
-          <nav className='pt-6 mt-6 border-t border-zink-900 flex flex-col gap-3'> 
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 1</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 2</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 3</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 4</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 5</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 6</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 7</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 8</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 9</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 10</a>
-          </nav>
-        </aside>
+        <Sidebar />
         <main className="flex-1 p-6">
           <div className='flex items-center gap-4'>
             <button className='rounded-full bg-black/40 p-1'>
@@ -101,25 +80,7 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 items-center justify-between">
-          <div className="flex items-center gap-2">
-            <Image src="/matue.jpeg" width={56} height={56} alt='Capa do album Máquina do Tempo' />
-            <div className="flex flex-col">
-                <strong className='font-normal'>Máquina do Tempo</strong>
-                <span>Matuê</span>
-            </div>
-          </div>
-          <div className='flex flex-col items-center'>
-            <div className='flex items-center justify-between'>
-              <Shuffle />
-              <SkipBack />
-              <button className='w-10 h-10 flex pl-0.5 items-center justify-center rounded-full bg-white text-black'>
-                <Play className=''/>
-              </button>
-              <SkipForward />
-            </div>
-          </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,31 @@
+import {Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize, Maximize2 } from 'lucide-react';
+import Image from 'next/image';
+
+export function Sidebar() {
+    return(
+        <aside className="w-72 bg-zinc-950 p-6">
+        <div className='flex items-center gap-2'>
+          <div className='w-3 h-3 bg-red-500 rounded-full' />
+          <div className='w-3 h-3 bg-yellow-500 rounded-full' />
+          <div className='w-3 h-3 bg-green-500 rounded-full' />
+        </div>
+        <nav className='space-y-5 mt-10'>
+          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'><HomeIcon />Home</a>
+          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'><Search />Search</a>
+          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'><Library />Your Library</a>
+        </nav>
+        <nav className='pt-6 mt-6 border-t border-zink-900 flex flex-col gap-3'> 
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 1</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 2</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 3</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 4</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 5</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 6</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 7</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 8</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 9</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist 10</a>
+        </nav>
+      </aside>
+    )
+}

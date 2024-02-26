2375dbc46a0467e9cc46c9ad2d989d64ae47cbd7
Matheus Oliveira <118443175+matheus-dev-fullstack@users.noreply.github.com>
2024-02-26 15:49:59 +0000

    Iniciando ajuste no Hover do Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -15,35 +15,35 @@ export function Sidebar() {
           <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'><Library />Sua Biblioteca</a>
         </nav>
         <nav className='pt-6 mt-6 border-t border-zink-900 flex flex-col gap-3 px-2'> 
-            <a className="flex items-center gap-3">
+            <a className="flex items-center gap-3 hover:bg-white/10 ">
               <Image src="/musicas-curtidas.png" className='rounded' width={42} height={42} alt='Capa do album Máquina do Tempo' />
               <div className="flex flex-col py-3">
                 <span className='font-semibold text-sm'>Músicas Curtidas</span>
                 <span className='text-sm text-zinc-400 p-0'>19 músicas</span>
               </div>
             </a>
-            <a className="flex items-center gap-3">
+            <a className="flex items-center gap-3 hover:bg-white/10 ">
               <Image src="/episodios.png" className='rounded' width={42} height={42} alt='Capa do album Máquina do Tempo' />
               <div className="flex flex-col">
                 <span className='font-semibold text-sm'>Seus episódios</span>
                 <span className='text-sm text-zinc-400'>Episódios sanvos e baixados</span>
               </div>
             </a>
-            <a className="flex items-center gap-3">
+            <a className="flex items-center gap-3 hover:bg-white/10 ">
               <Image src="/cartas-ao-remetente.jpeg" className='rounded' width={42} height={42} alt='Capa do album Máquina do Tempo' />
               <div className="flex flex-col">
                 <span className='font-semibold text-sm'>Cartas Ao Remetente</span>
                 <span className='text-sm text-zinc-400'>2014 - Rosa de Saron</span>
               </div>
             </a>
-            <a className="flex items-center gap-3">
+            <a className="flex items-center gap-3 hover:bg-white/10 ">
               <Image src="/radio-grao.jpeg" className='rounded' width={42} height={42} alt='Capa do album Máquina do Tempo' />
               <div className="flex flex-col">
                 <span className='font-semibold text-sm'>Rádio de Grão de Areia - Ao ...</span>
                 <span className='text-sm text-zinc-400'>Playlist - Spotfy</span>
               </div>
             </a>
-            <a className="flex items-center gap-3">
+            <a className="flex items-center gap-3 hover:bg-white/10 ">
               <Image src="/radio-quando-a-saudade.jpeg" className='rounded' width={42} height={42} alt='Capa do album Máquina do Tempo' />
               <div className="flex flex-col">
                 <span className='font-semibold text-sm'>Rádio de Quando A Saudade...</span>
